c6726becae2335a434b2293acbd8f4dc252890ca
codeReader52 <bluetaxi23@gmail.com>
2026-03-06 18:03:50 +1100

    init pygame

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 from Cell import Cell
-from Simulation import createGridWorld
+import pygame
 
-world = createGridWorld(20, 20)
+pygame.init()
+
+screen = pygame.display.set_mode([800, 800])
+
+white = [255, 255, 255]
+red = [255, 0, 0]
+
+while True:
+    event_list = pygame.event.get()
+    index = 0
+    while index < len(event_list):
+        event = event_list[index]
+        if event.type == pygame.MOUSEMOTION:
+            mouse_position = event.pos
+        if event.type == pygame.QUIT:
+            import sys
+            sys.exit(0)
+        index += 1
+
+    screen.fill(white)
+    index = 0
+    while index < 800 / 10:
+        pygame.draw.rect(screen, red, pygame.Rect(10 * index, 0, 10, 10))
+        index += 1
+
+    pygame.display.update()
