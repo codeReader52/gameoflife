c61d40a4b5a424b823e93b329fc0182fd3e4ec68
codeReader52 <bluetaxi23@gmail.com>
2026-05-22 17:44:52 +1000

    print neighbour

--- main.py
+++ main.py
@@ -11,15 +11,50 @@ def mousePositionToCellCoord(mouseCoord):
 pygame.init()
 screen = pygame.display.set_mode((800,800))
 
-great_depression= [194, 178, 178]
-b = [34, 5, 252]
-great_money = [98, 252, 3]
+grey = [194, 178, 178]
+blue = [34, 5, 252]
+green = [98, 252, 3]
 
 index = 0
 running = True
 # cells = createGridWorld(800/ 10, 800 / 10)
 
 cells = createGridWorld(5, 5)
+indexr = 0
+
+while indexr < len(cells):
+    row = cells[indexr]
+    indexc = 0
+    while indexc <len(row):
+        if indexr - 1 >= 0 and indexr - 1 < len(cells) and indexc - 1 >= 0 and indexc - 1 < len(row):
+            print(indexr-1, indexc-1)
+
+        if indexr - 1 >= 0 and indexr - 1 < len(cells) and indexc >= 0 and indexc < len(row):
+            print(indexr - 1, indexc)
+
+        if indexr - 1 >= 0 and indexr - 1 < len(cells) and indexc + 1 >= 0 and indexc + 1 < len(row):
+            print(indexr-1, indexc+1)
+
+        if indexr >= 0 and indexr < len(cells) and indexc - 1 >= 0 and indexc - 1 < len(row):
+            print(indexr, indexc-1)
+
+        if indexr >= 0 and indexr < len(cells) and indexc + 1 >= 0 and indexc + 1 < len(row):
+            print(indexr, indexc + 1)
+        
+        if indexr + 1 >= 0 and indexr + 1 < len(cells) and indexc - 1 >= 0 and indexc - 1 < len(row):
+            print(indexr+1, indexc-1)
+
+        if indexr + 1 >= 0 and indexr + 1 < len(cells) and indexc >= 0 and indexc < len(row):
+            print(indexr+1, indexc)
+
+        if indexr + 1 >= 0 and indexr + 1 < len(cells) and indexc + 1 >= 0 and indexc + 1 < len(row):
+            print(indexr+1, indexc+1)
+
+        print('-----------------------------------------------')
+        indexc += 1
+
+    indexr += 1
+
 # (3, 4) -> cells[3][4]
 
 # Iterate through each row in cells
@@ -27,33 +62,35 @@ cells = createGridWorld(5, 5)
 # Access the cell by index
 # For each of the cell, print out all indices of neighbours
 
-while running:
-    # Handle user events
-    eventl = pygame.event.get()
-    eventi= 0
-    while eventi <len (eventl):
-        event = eventl[eventi]
-        if event.type == pygame.QUIT:
-            running =False    
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mousepos = pygame.mouse.get_pos()
-            col_index = int(mousepos[0] / 10)
-            row_index = int(mousepos[1] / 10)
 
-            cells[row_index][col_index].isAlive = True
+
+# while running:
+#     # Handle user events
+#     eventl = pygame.event.get()
+#     eventi= 0
+#     while eventi <len (eventl):
+#         event = eventl[eventi]
+#         if event.type == pygame.QUIT:
+#             running =False    
+#         if event.type == pygame.MOUSEBUTTONDOWN:
+#             mousepos = pygame.mouse.get_pos()
+#             col_index = int(mousepos[0] / 10)
+#             row_index = int(mousepos[1] / 10)
+
+#             cells[row_index][col_index].isAlive = True
             
-        eventi = eventi + 1
+#         eventi = eventi + 1
 
-    # Drawing graphics
-    screen.fill(b)
-    index = 0
-    while index < len(cells):
-        tndex = 0
-        row =cells[index]
-        while tndex <len(row):
-            cell = row[tndex]
-            cell.draw(screen)
-            tndex += 1
-        index +=1
+#     # Drawing graphics
+#     screen.fill(blue)
+#     index = 0
+#     while index < len(cells):
+#         tndex = 0
+#         row =cells[index]
+#         while tndex <len(row):
+#             cell = row[tndex]
+#             cell.draw(screen)
+#             tndex += 1
+#         index +=1
 
-    pygame.display.update()
+#     pygame.display.update()
