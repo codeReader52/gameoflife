7f6cfc257a9d326e7e386fb819c515e68602e27b
codeReader52 <bluetaxi23@gmail.com>
2026-06-12 17:35:04 +1000

    drawing circles

--- boid/main.py
+++ boid/main.py
@@ -5,8 +5,11 @@ pygame.init()
 screen =pygame.display.set_mode((800,600))
 
 blue = [34, 5, 252]
+red = [255,0,0]
 
 running = True
+elements = []
+
 while running:
     ind1=0
     event_list = pygame.event.get()
@@ -15,10 +18,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            print(pygame.mouse.get_pos())
-
+            cords = pygame.mouse.get_pos()
+            if cords in elements:
+                print("This is already in the list")
+            else:
+                elements.append(pygame.mouse.get_pos())
+                print(elements)
         ind1 += 1
-        
 
     screen.fill(blue)
+    index = 0
+    while index < len(elements):
+        center = elements[index]
+        pygame.draw.circle(screen,red,center,5)
+        index += 1
     pygame.display.update()
