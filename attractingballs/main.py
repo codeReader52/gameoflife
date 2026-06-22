77eabe1bbac96fc48c1dc27ecdb8196f3473b7b1
codeReader52 <bluetaxi23@gmail.com>
2026-06-22 18:43:20 +1000

    wip

--- attractingballs/main.py
+++ attractingballs/main.py
@@ -1,12 +1,23 @@
 import pygame
 import random
+from datetime import datetime
 from Circle import Circle
+
+previous = datetime.now()
+def has_it_been(seconds):
+    global previous
+    now = datetime.now()
+
+    if (now - previous).seconds > seconds:
+        previous = now
+        return True
+    else:
+        return False
 
 pygame.init()
 
 screen = pygame.display.set_mode((500, 500))
 
-mouse_position = (250, 250)
 circle_radius = 5
 
 circles = []
@@ -17,7 +28,9 @@ while index < 500:
     circles.append(circle)
     index += 1
 
-while True:
+start_animation = False
+
+while True:    
     event_list = pygame.event.get()
     index = 0
     while index < len(event_list):
@@ -25,7 +38,24 @@ while True:
         if event.type == pygame.QUIT:
             import sys
             sys.exit(0)
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("You have started the Game")
+            start_animation = True
         index += 1
+
+    
+    benry = 0
+
+    # If start_animation is True, then: 
+    # Update all the circles so that their y coordinate is increased by 5px every second
+    if start_animation == True:
+        if has_it_been(1):
+            while benry < len(circles): 
+                tenry = circles[index]
+                tenry.y += 5
+                benry += 1
+
+
 
     screen.fill((255, 255, 255))
     
@@ -33,4 +63,5 @@ while True:
     while index < len(circles):
         circles[index].draw()        
         index += 1
+    
     pygame.display.update() 
