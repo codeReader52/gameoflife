27518098ec77fa965fd3c3fe10a345047e4861f1
codeReader52 <bluetaxi23@gmail.com>
2026-07-19 17:03:36 +1000

    current progress

--- attractingballs/main.py
+++ attractingballs/main.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from Circle import Circle
 
 previous = datetime.now()
-def has_it_been(seconds):
+def has_it_been(milliseconds):
     global previous
     now = datetime.now()
 
-    if (now - previous).seconds > seconds:
+    if (now - previous).total_seconds() * 1000 > milliseconds:
         previous = now
         return True
     else:
@@ -31,6 +31,8 @@ while index < 500:
 start_animation = False
 
 while True:    
+    max_w = 500
+    max_h = 500
     event_list = pygame.event.get()
     index = 0
     while index < len(event_list):
@@ -43,20 +45,36 @@ while True:
             start_animation = True
         index += 1
 
-    
-    benry = 0
-
     # If start_animation is True, then: 
     # Update all the circles so that their y coordinate is increased by 5px every second
     if start_animation == True:
-        if has_it_been(1):
-            while benry < len(circles): 
-                tenry = circles[index]
-                tenry.y += 5
-                benry += 1
+        if has_it_been(50):
+            index = 0
+            while index < len(circles): 
+                circle = circles[index]
+                # Make the circle move randomly, but maximally -10 to the left and 10 to the right, -10 up and 10 down
+                # circle.update_position(random.randint(-10, 10), random.randint(-10, 10))
+                dx, dy = random.randint(-10, 10), random.randint(-10, 10)
+                x, y = circle.x, circle.y
+                if x + dx < 0:
+                    x = abs(x + dx)
+                elif x + dx > max_w:
+                    x = 2 * max_w - x - dx
+                else:
+                    x = x + dx
 
+                if y + dy < 0:
+                    y = abs(y + dy)
+                elif y + dy > max_h:
+                    y = 2 * max_h - y - dy
+                else:
+                    y = y + dy
 
+                circle.x, circle.y = x, y
+                index += 1
 
+            
+                
     screen.fill((255, 255, 255))
     
     index = 0
