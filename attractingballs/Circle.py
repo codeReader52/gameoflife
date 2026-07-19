82ad2ec5fc7bf2683c886e935623e276fc30f5e6
codeReader52 <bluetaxi23@gmail.com>
2026-07-19 17:03:36 +1000

    current progress

--- attractingballs/Circle.py
+++ attractingballs/Circle.py
@@ -10,3 +10,8 @@ class Circle:
 
     def draw(self):
         pygame.draw.circle(self.screen, self.color, (self.x, self.y), self.radius)
+
+    def update_position(self, delta_x, delta_y):
+        self.x = self.x + delta_x
+        self.y = self.y + delta_y
+        
