9418655139c819d804ceee026356c626cd7f57f8
codeReader52 <bluetaxi23@gmail.com>
2026-09-06 18:07:58 +1000

    movement

--- mechanical2/main.py
+++ mechanical2/main.py
@@ -6,8 +6,28 @@ previous = datetime.now()
 SCREEN_SIZE = 800
 white = 255, 255, 255
 blue = 0, 0, 255
+red = 255, 0, 0
 green = 0, 255, 0
+green_circle_radius = 125
+green_circle_centre = [SCREEN_SIZE - 125, SCREEN_SIZE / 2]
+point_right_p = [green_circle_radius, 0]
+
+angle = 0
+delta_angle = 0.03
 screen = pygame.display.set_mode((SCREEN_SIZE, SCREEN_SIZE))
+
+def has_it_been(milliseconds):
+    global previous
+    now = datetime.now()
+    if (now - previous).total_seconds() * 1000 > milliseconds:
+        previous = now
+        return True
+    else:
+        return False
+
+def polar_to_cartesian(radius, thetha, translation = [SCREEN_SIZE/ 2, SCREEN_SIZE / 2]):
+    cat_coord = (radius * math.cos(thetha), -1 * radius * math.sin(thetha))
+    return [cat_coord[0] + translation[0], cat_coord[1] + translation[1]]
 
 running = True
 while running:
@@ -19,9 +39,13 @@ while running:
             running = False
         event_index += 1
 
-
-
+    if has_it_been(10):
+        angle = angle - delta_angle
+        green_circle_centre = polar_to_cartesian(275, angle)
+        
     screen.fill(white)
     pygame.draw.circle(screen, blue, [400, 400], 400)
-    pygame.draw.circle(screen, green, [100, 400], 100)
+    pygame.draw.circle(screen, green, green_circle_centre, 100)
+    
+    pygame.draw.circle(screen, red, [green_circle_centre[0], green_circle_centre[1] - 100] , 5)
     pygame.display.update()
